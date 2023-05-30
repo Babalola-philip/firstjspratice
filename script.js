cec5dba7d44e79a1bed7b64a21aab11cd3cf7ef7
lobos <phillipstimi387@gmail.com>
2023-05-30 10:01:35 +0100

    i added a new file

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Hello Lobos")
+
+
+//classwork 
+var number = Number(prompt("Enter your number"))
+if (number%2 ==0) {
+    console.log("Even Number");
+} else if (number%2 ==1) {
+    console.log("Old number");
+}
